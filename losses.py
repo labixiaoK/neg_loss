86dbe68317fb99824f7304e284747127be1c53a7
lehgtrung <43028892+lehgtrung@users.noreply.github.com>
2019-03-29 09:45:49 +0700

    Using expand instead of repeat

--- losses.py
+++ losses.py
@@ -6,16 +6,18 @@ import torch.nn as nn
 class NEGLoss(nn.Module):
     def __init__(self, dist, n_classes, n_samples, device):
         super(NEGLoss, self).__init__()
-        self.dist = dist
+
         self.n_samples = n_samples
         self.device = device
         self.n_classes = n_classes
 
-    def get_samples(self, targets):
-        if self.dist is not None:
-            dist = self.dist.repeat(targets.shape[0], 1)
+        if dist is not None:
+            self.dist = dist
         else:
-            dist = torch.Tensor([1/self.n_classes]).repeat(targets.shape[0], self.n_classes)
+            self.dist = torch.Tensor([1/self.n_classes]).repeat(self.n_classes)
+
+    def get_samples(self, targets):
+        dist = self.dist.expand(targets.shape[0], self.dist.shape[0])
         dist[range(targets.shape[0]), targets] = .0
         return torch.multinomial(dist, self.n_samples, replacement=True)
 
@@ -28,7 +30,7 @@ class NEGLoss(nn.Module):
         log_targets = probs[rng, targets].sigmoid().log().sum()
 
         # Log samples
-        idx = torch.arange(batch_size).repeat(self.n_samples, 1).transpose(0, 1)
+        idx = torch.arange(batch_size).expand(self.n_samples, batch_size).transpose(0, 1)
         log_samples = probs[idx, negs].neg().sigmoid().log().sum()
 
         return - (log_targets + log_samples) / batch_size
